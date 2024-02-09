a5de65be483e4d5019227e48232b844607d48869
marcovittoriozacca <zaccamarco01@gmail.com>
2024-02-09 13:42:46 +0100

    feat: App Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script>
+import { store } from './store';
+import AppHeader from './components/header/AppHeader.vue'
+
+    export default {
+        components:{
+            AppHeader,
+        },
+
+        data() {
+            return {
+                store
+            }
+        },
+        mounted() {}
+    }
 
 </script>
 
 <template>
-  
+
+    <header>
+        <AppHeader/>
+    </header>
+    
 </template>
 
+
 <style lang="scss">
+@use './assets/style/general'
 
 </style>
---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name: 'AppHeader',
+    setup () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <figure>
+            <img src="../../assets/img/yu-gi-oh-logo.jpg" alt="logo-da-inserire">
+        </figure>
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../../assets/style/partials/mixins' as *;
+
+div{
+    background-color: white;
+    padding: 15px 0;
+    @include flex-align-center;
+
+    figure{
+        @include flex-align-center;
+        width: 80px;
+        margin-inline: 15px;
+
+        img{
+            width: 100%;
+        }
+    }
+
+    h1{
+        font-weight: 400;
+    }
+}
+
+</style>
